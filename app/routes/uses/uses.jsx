5fe9bbe9e640bea55c02b86e816db6098be591db
H3ldex <helderscarab@gmail.com>
2025-01-23 04:21:28 +0000

    Update Uses page: change background video, refine tool descriptions, and update system specifications
    - Replaced the background video with a new asset (VZgreen.mp4).
    - Simplified the description of the "Uses" section by removing references to a GIF.
    - Updated various tool descriptions for clarity and added links to projects.
    - Revised system specifications, including updates to desktop, operating system, browser, monitor, keyboard, mouse, laptop, headphones, and microphone details.

--- app/routes/uses/uses.jsx
+++ app/routes/uses/uses.jsx
@@ -1,5 +1,5 @@
 import usesBackgroundPlaceholder from '~/assets/uses-background-placeholder.jpg';
-import usesBackground from '~/assets/uses-background.mp4';
+import usesBackground from '~/assets/VZgreen.mp4';
 import { Footer } from '~/components/footer';
 import { Link } from '~/components/link';
 import { List, ListItem } from '~/components/list';
@@ -35,7 +35,7 @@ export const Uses = () => {
         />
         <ProjectHeader
           title="Uses"
-          description="A somewhat comprehensive list of tools, apps, hardware, and more that I use on a daily basis to design and code things. And yeah, that is a Johnny Mnemonic GIF in the background."
+          description="A somewhat comprehensive list of tools, apps, hardware, and more that I use on a daily basis to design and code things."
         />
         <ProjectSection padding="none" className={styles.section}>
           <ProjectSectionContent>
@@ -44,20 +44,20 @@ export const Uses = () => {
               <ProjectSectionText as="div">
                 <List>
                   <ListItem>
-                    <Link href="https://www.figma.com">Figma</Link> is my primary tool for
-                    UI design these days. Made the switch from Sketch in 2020 and haven’t
-                    looked back. I’ve also created{' '}
-                    <Link href="https://www.figma.com/@hamish">a few plugins</Link> that
-                    you can install.
+                  <Link href="https://www.figma.com">Figma</Link> is my primary tool for
+                    UI design these days. Made the switch from Sketch in 2020 and haven't
+                    looked back. Check my portfolio{' '}
+                    <Link href="https://www.figma.com/design/64zsRxhnMm84wIkNrMva1g/H3ldex-Portfolio?node-id=2301-92&t=3OBV3xF5Q63lhKNK-1">a few projects</Link> that
+                    i did in the web2 and web3 space.
                   </ListItem>
                   <ListItem>
                     Any motion graphics I create are created in Adobe After Effects. So
-                    far I haven’t found a non-Adobe product that’s as good. If anyone has
+                    far I haven't found a non-Adobe product that's as good. If anyone has
                     suggestions please <Link href="/contact">message me</Link>.
                   </ListItem>
                   <ListItem>
                     For any 3D models and video editing I use{' '}
-                    <Link href="https://www.blender.org/">Blender</Link>. Since 2.8 it’s
+                    <Link href="https://www.blender.org/">Blender</Link>. Since 2.8 it's
                     become way simpler to use and in a lot of ways better than expensive
                     paid tools like 3DS Max or Maya.
                   </ListItem>
@@ -73,9 +73,8 @@ export const Uses = () => {
               <ProjectSectionText as="div">
                 <List>
                   <ListItem>
-                    I use <Link href="https://vscodium.com/">VSCodium</Link> as my text
-                    editor, with the Tokyo Night theme and Operator Mono as my typeface of
-                    choice.
+                  I use <Link href="https://www.cursor.com/">Cursor</Link> as my text
+                    editor, Cursor is the best way to code with AI.
                   </ListItem>
                   <ListItem>
                     Firefox is my main browser for both development and general use.
@@ -91,23 +90,20 @@ export const Uses = () => {
                     learning curve but you can do some really powerful stuff with it.
                   </ListItem>
                   <ListItem>
-                    For CSS I’ve used a myriad pre-processors and css-in-js solutions like
-                    styled-components, but these days I’m using vanilla CSS with{' '}
+                    For CSS I've used a myriad pre-processors and css-in-js solutions like
+                    styled-components, but these days I'm using vanilla CSS with{' '}
                     <Link href="https://postcss.org/">PostCSS</Link> to get upcoming CSS
                     features today.
                   </ListItem>
                   <ListItem>
                     For Javascript animations I use{' '}
-                    <Link href="https://www.framer.com/motion/">Framer Motion</Link>, it’s
+                    <Link href="https://www.framer.com/motion/">Framer Motion</Link>, it's
                     a great way to add spring animations to React and three.js.
                   </ListItem>
                   <ListItem>
-                    For building and testing UI components in isolation I use{' '}
-                    <Link href="https://storybook.js.org/">Storybook</Link>. Check out the{' '}
-                    <Link href="https://storybook.hamishw.com">
-                      storybook for this website
-                    </Link>
-                    .
+                    For Designing Illustrations I use{' '}
+                    <Link href="https://www.adobe.com/">Adobe Illustrator</Link>, and{' '}
+                    <Link href="https://www.figma.com/">Figma</Link>.
                   </ListItem>
                 </List>
               </ProjectSectionText>
@@ -119,40 +115,40 @@ export const Uses = () => {
             <ProjectTextRow stretch width="m">
               <ProjectSectionHeading>System</ProjectSectionHeading>
               <Table>
-                <TableBody>
-                  <TableRow>
+              <TableBody>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Desktop</TableHeadCell>
-                    <TableCell>Custom built</TableCell>
+                    <TableCell>ASUS ROG Strix G16CH-1370KF0960</TableCell>
                   </TableRow>
-                  <TableRow>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Operating system</TableHeadCell>
-                    <TableCell>Arch Linux (by the way)</TableCell>
+                    <TableCell>Intel Core i7-13700KF</TableCell>
                   </TableRow>
-                  <TableRow>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Browser</TableHeadCell>
-                    <TableCell>Zen Browser</TableCell>
+                    <TableCell>Firefox</TableCell>
                   </TableRow>
-                  <TableRow>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Monitor</TableHeadCell>
-                    <TableCell>1440p IPS 144hz LG 27GL850</TableCell>
+                    <TableCell>ROG Swift OLED PG27UCDM</TableCell>
                   </TableRow>
-                  <TableRow>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Keyboard</TableHeadCell>
-                    <TableCell>Tofu65</TableCell>
+                    <TableCell>Apex Pro Gen3</TableCell>
                   </TableRow>
-                  <TableRow>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Mouse</TableHeadCell>
-                    <TableCell>Logitech G403</TableCell>
+                    <TableCell>Razer Viper Ultimate</TableCell>
                   </TableRow>
-                  <TableRow>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Laptop</TableHeadCell>
-                    <TableCell>Macbook Pro 14″</TableCell>
+                    <TableCell>Asus ROG Zephyrus G14</TableCell>
                   </TableRow>
-                  <TableRow>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Headphones</TableHeadCell>
-                    <TableCell>Audio Technica ATH-M50x/Apple Airpods</TableCell>
+                    <TableCell>Airpods Air</TableCell>
                   </TableRow>
-                  <TableRow>
+                  <TableRow className={styles.tableRow}>
                     <TableHeadCell>Microphone</TableHeadCell>
                     <TableCell>Blue Yeti</TableCell>
                   </TableRow>
